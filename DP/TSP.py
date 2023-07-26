a8f39be2b077f1736dae718073610a3a299d8223
LMeyling <113123663+LMeyling@users.noreply.github.com>
2023-07-26 16:11:45 +0200

    Update TSP.py

--- DP/TSP.py
+++ DP/TSP.py
@@ -1,12 +1,16 @@
-M={}
+M={};Z={}
 N=frozenset(range(1,len(dist_m)))
 def dist(ni,N):
     if not N:
-        return dist_m[ni][0]
-    c=[(nj,dist_m[ni][nj]+dist(nj,N.difference({nj}))) for nj in N]
+        Z[(ni,N)]=dist_m[ni][0]
+        return
+    for nj in N:
+        if (nj,N.difference({nj})) not in Z:
+            dist(nj,N.difference({nj}))
+    c=[(nj,dist_m[ni][nj]+Z[(nj,N.difference({nj}))]) for nj in N]
     nmin,min_cost=min(c,key=lambda x:x[1])
     M[(ni,N)] = nmin
-    return min_cost
+    Z[(ni,N)] = min_cost
 min_dist = dist(0,N)
 ni = 0
 solution = [0]
@@ -15,3 +19,4 @@ while N:
     ni = M[(ni, N)]
     solution.append(ni)
     N = N.difference({ni})
+print(solution)

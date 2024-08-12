0d66a3c008d7974fdd6bd462936b265669e944e1
LMeyling <113123663+LMeyling@users.noreply.github.com>
2024-08-12 11:50:09 +0200

    added lineDist and lineInter

---
+++ GEONEO/lineDist.py
@@ -0,0 +1,6 @@
+# returns signed distance between p and line from a to b
+
+from point import *
+
+def lineDist(a,b,p):
+    return (b-a).cross(p-a)/(b-a).dist()
---
+++ GEONEO/lineInter.py
@@ -0,0 +1,11 @@
+# returns Intersection of two lines (s1->e1) and (s2->e2)
+# if one Intersection 1, Point; if no Intersection 0, (0,0); if infinite -1, (0,0)
+
+from point import *
+
+def lineInter(s1,e1,s2,e2):
+    d = (e1-s1).cross(e2-s2)
+    if d == 0:
+        return -(s1.crossp(e1,s2) == 0), Point(0,0)
+    p = s2.crossp(e1,e2); q = s2.crossp(e2,s1)
+    return 1, (s1 * p + e1 * q) / d
